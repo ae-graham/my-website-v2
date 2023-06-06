e2bf2d18e58f632b210c7e186865ae33999494fc
Alex Graham <ae.graham93@gmail.com>
2023-06-06 15:30:57 +1000

    removed the travel theme, added road background

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,34 @@
+
+
 const About = () => {
   return(
-    <section className='about-container'>
-      <div className='about-item'>
-        <h1>About Me</h1>
-        <p>Hey! my name is Alex Graham, I have a strong background in building and commercializing digital solutions for engineering applications.</p>
-        <p>I graduated as an Electronics Engineer and have spent the last 5 years working in numerous industries such as mining, electronics manufacturing and retail. My passion for creating and building UIs in these sectors, led me to shift my focus on to web-development in 2022.</p>
-        <p>I'm an organised, committed and reliable professional. Great team player, able to work under pressure, determined, and good at multitasking. When I'm not working I tend to travel a lot. I've visited approximately 50 countries all around the world, and lived in several of them, including Spain, UK, Denmark and now Australia.</p>
-        <p>I like to spend my free time working out, looking into blockchain technology & cryptocurrencies, and occassionally hit the slopes during winter as I'm an advanced snowboarder.</p>
-      </div>
-      <div>
-
-      </div>
-    </section>
+    <div className='about-container'>
+      <section className='about-section'>
+        <div className='about-item'>
+          <h1>what I have done</h1>
+          <p>Strong background in building and commercializing digital solutions for engineering applications.</p>
+          <p>Graduated as an Electronics Engineer and spent the last 5 years working in numerous industries such as mining, electronics manufacturing and retail. My passion for creating and building UIs in these sectors, led me to shift my focus on to web-development in 2022.</p> 
+        </div>
+        <div className='about-item'>
+          <h1>how I see myself</h1>
+          <p>Organised, committed and reliable professional. Great team player, able to work under pressure, determined, and good at multitasking.</p>
+        </div>
+        <div className='about-item'>
+          <h1>what I like doing</h1>
+          <p>When I'm not working I tend to travel a lot. I've visited approximately 50 countries all around the world, and lived in several of them, including Spain, UK, Denmark and now Australia.</p>
+          <p>I like to spend my free time working out, looking into blockchain technology & cryptocurrencies, and occassionally hit the slopes during winter as I'm an advanced snowboarder.</p>
+        </div>
+      </section>
+    </div>
   )
 }
 
 export default About;
+
+/*
+  <h1>About Me</h1>
+  <p>Hey! my name is Alex Graham, I have a strong background in building and commercializing digital solutions for engineering applications.</p>
+  <p>I graduated as an Electronics Engineer and have spent the last 5 years working in numerous industries such as mining, electronics manufacturing and retail. My passion for creating and building UIs in these sectors, led me to shift my focus on to web-development in 2022.</p>
+  <p>I'm an organised, committed and reliable professional. Great team player, able to work under pressure, determined, and good at multitasking. When I'm not working I tend to travel a lot. I've visited approximately 50 countries all around the world, and lived in several of them, including Spain, UK, Denmark and now Australia.</p>
+  <p>I like to spend my free time working out, looking into blockchain technology & cryptocurrencies, and occassionally hit the slopes during winter as I'm an advanced snowboarder.</p>
+*/
