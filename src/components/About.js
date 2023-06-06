b4f35600189c2f77dc7c98c2cdd10df84bcccdad
Alex Graham <ae.graham93@gmail.com>
2023-06-06 20:24:06 +1000

    added transition to about image

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,32 @@
+import portrait from '../assets/portrait.png';
+
+const handleHover = () => {
+  const outline = document.getElementById('outline');       //I use this handleHover function to add style to the 'outline div' when hovering over the image.
+  const photo = document.getElementById('photo');
+  photo.addEventListener('mouseover', () => {
+    outline.style.translate = '-10px 10px';
+  })
+  photo.addEventListener('mouseleave', () => {
+    outline.style.translate = '0px 0px';
+  })
+  
+}
+
 
 
 const About = () => {
   return(
     <div className='about-container'>
-      <section className='about-section'>
+      <section id='about-section'>
         <div className='about-item'>
-          <h1>what I have done</h1>
-          <p>Strong background in building and commercializing digital solutions for engineering applications.</p>
-          <p>Graduated as an Electronics Engineer and spent the last 5 years working in numerous industries such as mining, electronics manufacturing and retail. My passion for creating and building UIs in these sectors, led me to shift my focus on to web-development in 2022.</p> 
+          <img id='photo' src={portrait} alt='portrait of Alex' onMouseEnter={handleHover} onMouseLeave={handleHover}/>
+          <div id='outline'></div>
         </div>
         <div className='about-item'>
-          <h1>how I see myself</h1>
+          <h1>what I've done</h1>
+          <p>Strong background in building and commercializing digital solutions for engineering applications.</p>
+          <p>Graduated as an Electronics Engineer and spent the last 5 years working in numerous industries such as mining, electronics manufacturing and retail. My passion for creating and building UIs in these sectors, led me to shift my focus on to web-development in 2022.</p> 
+          <h1>how others see me</h1>
           <p>Organised, committed and reliable professional. Great team player, able to work under pressure, determined, and good at multitasking.</p>
         </div>
         <div className='about-item'>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,21 @@
 import '../css/Nav.css'
 
 const Nav = () => {
+  
+  const handleClick = (anchor) => () => {
+    const id = `${anchor}-section`;
+    const element = document.getElementById(id);
+    if(element) {
+      element.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  }
+
   return(
     <nav>
-      <a href='/#'>About</a>
+      <a href='#about' onClick={handleClick('about')}>About</a>
       <a href='/#'>Skills</a>
       <a href='/#'>Experience</a>
       <a href='/#'>Work</a>
